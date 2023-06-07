573e12f9010395776fe1a2970fdb90fc634e7e24
Jon Pillay <jonpillay@MASTUDENT-Jon-Pillay-C02G5H47Q6L4.local>
2023-06-07 15:16:24 +0100

    All DS functions working, however when trying to run through the site getting Could Not Proxy Request

--- api/clients/DSclient.js
+++ api/clients/DSclient.js
@@ -8,9 +8,10 @@ const apiKey = process.env.DS_KEY;
 
 if (!apiKey) throw new Error('Missing Stability API key.');
 
-const context_prompts = 'Matilda walking through the quaint streets of the village. A magnificent apothecary filled with sparkling potions and magical herbs. The wizard running the shop chatting with Matilda. Matilda selecting the perfect ingredients for her soup. The enchanting world surrounding Matilda.'
+// const context_prompts = 'Matilda walking through the quaint streets of the village. A magnificent apothecary filled with sparkling potions and magical herbs. The wizard running the shop chatting with Matilda. Matilda selecting the perfect ingredients for her soup. The enchanting world surrounding Matilda.'
 
-async function generateImage(prompts, style) {
+async function DSGenerateImage(prompts, style) {
+  // console.log(prompts)
   const response = await fetch(`${apiHost}/v1/generation/${engineId}/text-to-image`, {
     method: 'POST',
     headers: {
@@ -40,24 +41,34 @@ async function generateImage(prompts, style) {
 
   const responseJSON = await response.json();
   responseJSON.artifacts.forEach(async (image, index) => {
-    console.log("upload started")
-    const remoteImage = await imgBBUploader(process.env.IMGBB_KEY, image.base64)
+    // console.log("upload started")
     fs.writeFileSync(
       `./out/v1_txt2img_${Date.now()}.png`,
       Buffer.from(image.base64, 'base64')
     );
-    console.log(remoteImage.data.display_url)
+    const remoteImage = await imgBBUploader(process.env.IMGBB_KEY, image.base64)
+    // console.log(remoteImage.data.display_url)
     return remoteImage.data.display_url
   });
 }
 
-prompts = DSPromptGen({genre: 'fairytale', art_style: 'neon-punk'})
+// userinput = {
+//   character: 'Spiderman',
+//   genre: 'Fairytale',
+//   style: 'Realistic',
+//   prompt: 'go to the shops',
+//   messageHistory: [],
+//   imageHistory: []
+// }
 
-const context = "Matilda and her children walking through the misty, cobblestone streets. Matilda wearing a cloak and carrying a wand. The gates of Hogwarts in the background. The magical world feeling alive and full of possibilities."
+// const context = "Matilda and her children walking through the misty, cobblestone streets. Matilda wearing a cloak and carrying a wand. The gates of Hogwarts in the background. The magical world feeling alive and full of possibilities."
 
-// testing script
+// // testing script
 
 // console.log(context + prompts.prompts);
 
+// console.log(DSPromptGen(userinput))
 
-generateImage(context + prompts.prompts, prompts.art_style);
+// const prompts = DSPromptGen(userinput)
+
+// console.log(DSGenerateImage(prompts.prompts, prompts.art_style))
