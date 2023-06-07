0aa37bcf8625b6a91fbc917979252d800c20704a
Jon Pillay <jonpillay@MASTUDENT-Jon-Pillay-C02G5H47Q6L4.local>
2023-06-07 11:17:04 +0100

    commit before DreamStudio integration

--- frontend/src/components/result-page/ResultPage.js
+++ frontend/src/components/result-page/ResultPage.js
@@ -4,7 +4,7 @@ import Story from "../story/Story";
 import "./ResultPage.css";
 import LoadingIcon from "../loading-icon/LoadingIcon";
 import SteerStory from "../steer-story/SteerStory"
-import HomeIcon from "./home-icon.png"
+// import HomeIcon from "./home-icon.png"
 import HomeButton from "../home-button/HomeButton";
 
 const ResultPage = ({ navigate }) => {
