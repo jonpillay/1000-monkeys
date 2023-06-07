2d35e7b6fb583fdab15e2ba415854425f1070aea
Jon Pillay <jonpillay@MASTUDENT-Jon-Pillay-C02G5H47Q6L4.local>
2023-06-07 15:16:24 +0100

    All DS functions working, however when trying to run through the site getting Could Not Proxy Request

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -1,11 +1,17 @@
 const client = require("../clients/sdclient");
 const { matchSelectionCriteria } = require('../prompts/prompts')
+const DSGenerateImage = require("../clients/DSclient")
+const fs = require('fs');
+const DSPromptGen = require('../prompts/DSPromptGen')
+const imgBBUploader = require('../helpers/imgBBUploader')
+
 
 const ImagesController = {
   Index: async (req, res) => {
     try {
-      const prompts = matchSelectionCriteria(req.body)    
-      const result = await client.generateImage(prompts.positivePrompts, prompts.negativePrompts);
+      const prompts = DSPromptGen(req.body)
+      console.log(prompts)  
+      const result = await DSGenerateImage(prompts.prompts, prompts.art_style);
       res.status(200).json({ imgUrl: result });
     } catch (error) {
       res.status(error.status).json({ message: error.message });
