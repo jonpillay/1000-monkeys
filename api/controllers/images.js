0b318c12379c4fd62acb0f4ead49741231687e9e
Jon Pillay <jon.pillay@gmail.com>
2023-06-13 15:57:17 +0100

    console logs and in-code testing scripts removed

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -1,18 +1,13 @@
 const client = require("../clients/sdclient");
-const { matchSelectionCriteria } = require('../prompts/prompts')
 const DSGenerateImage = require("../clients/DSclient")
-const fs = require('fs');
 const DSPromptGen = require('../prompts/DSPromptGen')
-const imgBBUploader = require('../helpers/imgBBUploader')
 
 
 const ImagesController = {
   Index: async (req, res) => {
     try {
       const prompts = DSPromptGen(req.body)
-      console.log(prompts)  
       const result = await DSGenerateImage(prompts.prompts, prompts.art_style);
-      console.log(`This is the img url on the backend ${result}`)
       res.status(200).json({ imgUrl: result });
     } catch (error) {
       res.status(error.status).json({ message: error.message });
