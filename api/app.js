f431a375f4c6f7e2ae533d1e75de75d002bbd29f
Jon P <jon.pillay@gmail.com>
2023-08-02 11:35:47 +0100

    databasse connection setup, however I think it needs to be moved out into it's own folder/file

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const mongoose = require('mongoose')
 
 const ImagesRouter = require('./routes/images');
 const StoryRouter = require("./routes/story");
@@ -21,6 +24,10 @@ app.use("/images", ImagesRouter)
 app.use("/story", StoryRouter)
 app.use("/populate", PopulateRouter)
 
+// connect to db
+
+const PORT = process.env.DB_PORT || 5050;
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
@@ -35,5 +42,15 @@ app.use((err, req, res) => {
   // respond with details of the error
   res.status(err.status || 500).json({message: 'server error'})
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("connection to db successful")
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+})
 
 module.exports = app;
