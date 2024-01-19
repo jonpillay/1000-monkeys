df904fe946e8b8f016a4cd36b0594479353801fa
Jon P <jon.pillay@gmail.com>
2024-01-19 06:41:55 +0000

    storyBookReducer added to store

--- frontend/src/redux-state/store.js
+++ frontend/src/redux-state/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import storyBookReducer from '../components/story-book/storyBookSlice' 
+
 export const store = configureStore({
   reducer: {
-    
+    storyBookReducer,
   }
 })
