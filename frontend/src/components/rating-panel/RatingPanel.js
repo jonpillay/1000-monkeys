ab2af116c34c0a26aeafb342fb801368d2303e00
Jon P <jon.pillay@gmail.com>
2024-03-23 13:10:43 +0000

    some styling and scaffolding done on the RatingPanel. Commit before installing packages for animated number scroll for the rating UI.

--- frontend/src/components/rating-panel/RatingPanel.js
+++ frontend/src/components/rating-panel/RatingPanel.js
@@ -4,16 +4,16 @@ function RatingPanel(props) {
 
   const ratings = props.ratings
 
-  const rating = rating.length > 0 ? Math.floor((rating/rating.length) * 2) / 2 : 0
+  const rating = ratings.length > 0 ? Math.floor((rating/rating.length) * 2) / 2 : 4.5
 
   return (
     <div className="rating-panel-container">
       <div className="rating-panel-grid">
         <div className="rating-container">
-
+          {rating}
         </div>
         <div className="rate-verb-container">
-
+          Hello!
         </div>
       </div>
     </div>
