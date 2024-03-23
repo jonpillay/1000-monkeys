b6f052f01e5b2b91ffec12e593c8100e74171ec6
Jon P <jon.pillay@gmail.com>
2024-03-23 16:32:39 +0000

    basic functionality added to the voting counter

--- frontend/src/components/rate-story-panel/RateStoryPanel.js
+++ frontend/src/components/rate-story-panel/RateStoryPanel.js
@@ -1,20 +1,24 @@
 import "./RateStoryPanel.css"
 
+import { useState } from "react";
+
 function RateStoryPanel(props) {
+
+  const [currentRate, setCurrentRate] = useState(0)
 
   return (
     <div className="rate-story-panel-container">
       <div className="rate-story-grid">
         <div className="rate-count-container">
-          5
+          {currentRate}
         </div>
         <div className="inc-arrows-container">
           <div className="inc-arrows-grid">
             <div className="inc-button-container">
-              u
+              <button onClick={()=>{setCurrentRate(currentRate+1)}}>U</button>
             </div>
             <div className="inc-button-container">
-              d
+            <button onClick={()=>{setCurrentRate(currentRate-1)}}>D</button>
             </div>
           </div>
         </div>
