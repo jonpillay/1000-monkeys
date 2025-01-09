709ba1a36b1fd7d3a5ed7bee317d77db3ccc3ca0
Jon P <jon.pillay@gmail.com>
2025-01-09 20:16:13 +0000

    created WarningSplash component

---
+++ frontend/src/components/SplashPageParts/warning-splash/WarningSplash.js
@@ -0,0 +1,19 @@
+import './WarningSplash.css'
+
+const WarningSplash = () => {
+
+  return (
+    <div className="warning-splash-container">
+      <div className="warning-splash-grid">
+        <div className="warning-splash-body">
+          
+        </div>
+        <div className="warning-splash-text">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WarningSplash;
