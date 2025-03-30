33f84eeb38bd993a6a8174ba097ba15f5119e773
Jon P <jon.pillay@gmail.com>
2025-03-30 20:32:35 +0100

    added limiter to backend to protect sysInit routes

--- api/routes/systemInit.js
+++ api/routes/systemInit.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const SysInitController = require("../controllers/sysInitController");
+const rateLimit = require("express-rate-limit");
+const requestIp = require('request-ip');
 
-router.post("/", SysInitController.InitialiseSystem);
+router.use(requestIp.mw());
+
+const sysInitLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 50,
+    message: "Too many requests, please try again later.",
+    keyGenerator: (req) => req.clientIp || req.ip,
+  });
+
+router.post("/", sysInitLimiter, SysInitController.InitialiseSystem);
 
 module.exports = router;
