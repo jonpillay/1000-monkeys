301fd57640a400a6ef4ad3da9c43e14e9e2beb47
Jon P <jon.pillay@gmail.com>
2025-04-06 11:16:22 +0100

    added a no origin rule to the cors options

--- api/config/corsOptions.js
+++ api/config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || process.env.NODE_ENV !== "production") {
+    if (!origin || allowedOrigins.includes(origin) || process.env.NODE_ENV !== "production") {
       callback(null,true)
     } else {
       callback(new Error('Blocked By CORS'))
