ce6c6cf2def277f63698619017008b43f9e112c2
Jon P <jon.pillay@gmail.com>
2025-04-07 18:35:04 +0100

    fixed bug in image refresh caused by previous commit

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -25,7 +25,7 @@ const ImagesController = {
 
       const DS_descpription = await DSDescriptionGen(story_text, user_choices["genre"], user_choices["character"]) // needs 'system_prompts, chapter, genre, main_character' story text here needs to be only the content, not the full JSON object
 
-      const dressed_prompt = DCPromptDresser(DS_descpription, user_choices)
+      const dressed_prompt = await DCPromptDresser(DS_descpription, user_choices)
 
       const tagsObject = await genPromptTags(user_choices)
 
