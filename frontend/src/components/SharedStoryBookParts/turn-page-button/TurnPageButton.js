e3195e7cd0dd1553fb0f9ba81173e5e9a1739893
Jon P <jon.pillay@gmail.com>
2025-05-31 14:54:43 +0100

    fixed a layout bug in browse storybook container caused by the conditional rendering of the turn page buttons

--- frontend/src/components/SharedStoryBookParts/turn-page-button/TurnPageButton.js
+++ frontend/src/components/SharedStoryBookParts/turn-page-button/TurnPageButton.js
@@ -6,7 +6,9 @@ import BackButton from "../../../img/back_arrow2.png"
 const TurnPageButton = (props) => {
 
   return (
-    <input src={props.direct == "next" ? ForwardButton : BackButton} type="image" className="turn-page-button" id={props.id} onClick={() => props.callback([props.direct])}></input>
+    <div className="turn-page-button-container">
+      <input src={props.direct == "next" ? ForwardButton : BackButton} type="image" className="turn-page-button" id={props.id} onClick={() => props.callback([props.direct])}></input>
+    </div>
   )
 }
 
