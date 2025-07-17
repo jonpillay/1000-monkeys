64273386c84f6e3a9c06ced9237c5b1e479bcf00
Jon P <jon.pillay@gmail.com>
2025-07-17 16:14:47 +0100

    small changes to mobile splash page

--- frontend/src/components/MobileSplashPage/MobileSplashPage.js
+++ frontend/src/components/MobileSplashPage/MobileSplashPage.js
@@ -2,17 +2,14 @@ import './MobileSplashPage.css';
 
 const MobileSplashPage = (props) => {
 
-  const mobileSplashText = "The 1000 Monkeys App Has Been Optimised To Run In A Desktop Environment\n Head Over To Your Desktop or Laptop To Check It out"
-
-  // const mobileCTAtext = "Or Head Over To My <a href='https://github.com/jonpillay' target="_blank" rel="noopener noreferrer">GitHub</a> Or <a href='https://www.linkedin.com/in/jonpillay/' target="_blank" rel="noopener noreferrer">Linkedin</a>\nTo Find Out More About Me (The Dev)"
-
   return (
     <div className="mobile-splash-container">
       <div className="mobile-splash-title">
         Hello Mobile User!
       </div>
       <div className='mobile-splash-text-container'>
-        {mobileSplashText}
+        <div>The 1000 Monkeys App Has Been Optimised To Run In Desktop Browsers</div>
+        <div>Grab Your Laptop or Keyboard to Check It Out</div>
       </div>
       <div className='mobile-splash-page-cta'>
         <div>Or Head Over To My <a href='https://github.com/jonpillay' target="_blank" rel="noopener noreferrer">GitHub</a> Or <a href='https://www.linkedin.com/in/jonpillay/' target="_blank" rel="noopener noreferrer">Linkedin</a></div>
@@ -20,7 +17,7 @@ const MobileSplashPage = (props) => {
       </div>
       <div className='mobile-splash-page-video-cta'>
         <div>Check Out A video Introduction to 1000M</div>
-        <div> <a href='https://gumlet.tv/playlist/684584bf0f8d7a0518536218/' target="_blank" rel="noopener noreferrer">here</a></div>
+        <div> <a href='https://gumlet.tv/playlist/684584bf0f8d7a0518536218/' target="_blank" rel="noopener noreferrer">HERE</a></div>
       </div>
     </div>
   )
