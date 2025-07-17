0fc69fef1eb1354d1622c513f5a6ecb31a227c99
Jon P <jon.pillay@gmail.com>
2025-07-17 16:05:40 +0100

    styling change to mobile splash page

--- frontend/src/components/MobileSplashPage/MobileSplashPage.js
+++ frontend/src/components/MobileSplashPage/MobileSplashPage.js
@@ -2,22 +2,25 @@ import './MobileSplashPage.css';
 
 const MobileSplashPage = (props) => {
 
+  const mobileSplashText = "The 1000 Monkeys App Has Been Optimised To Run In A Desktop Environment\n Head Over To Your Desktop or Laptop To Check It out"
+
+  // const mobileCTAtext = "Or Head Over To My <a href='https://github.com/jonpillay' target="_blank" rel="noopener noreferrer">GitHub</a> Or <a href='https://www.linkedin.com/in/jonpillay/' target="_blank" rel="noopener noreferrer">Linkedin</a>\nTo Find Out More About Me (The Dev)"
+
   return (
     <div className="mobile-splash-container">
       <div className="mobile-splash-title">
         Hello Mobile User!
       </div>
       <div className='mobile-splash-text-container'>
-        The 1000 Monkeys App Has Been Optimised To Run In A Desktop Environment
-        Head Over To Your Desktop or Laptop To Check It out
+        {mobileSplashText}
       </div>
       <div className='mobile-splash-page-cta'>
-        Or Head Over To My <a href='https://github.com/jonpillay' target="_blank" rel="noopener noreferrer">GitHub</a> Or <a href='https://www.linkedin.com/in/jonpillay/' target="_blank" rel="noopener noreferrer">Linkedin</a>
-        To Find Out More About Me (The Dev)
+        <div>Or Head Over To My <a href='https://github.com/jonpillay' target="_blank" rel="noopener noreferrer">GitHub</a> Or <a href='https://www.linkedin.com/in/jonpillay/' target="_blank" rel="noopener noreferrer">Linkedin</a></div>
+        <div>To Find Out More About Me (The Dev)</div>
       </div>
       <div className='mobile-splash-page-video-cta'>
-        Check Out A video Introduction to 1000M <a href='https://gumlet.tv/playlist/684584bf0f8d7a0518536218/' target="_blank" rel="noopener noreferrer">here</a>
-        To Find Out More About Me (The Dev)
+        <div>Check Out A video Introduction to 1000M</div>
+        <div> <a href='https://gumlet.tv/playlist/684584bf0f8d7a0518536218/' target="_blank" rel="noopener noreferrer">here</a></div>
       </div>
     </div>
   )
