9857148b3d33869e80215d17426338c9d4341ebc
Jon P <jon.pillay@gmail.com>
2025-07-17 21:05:18 +0100

    added typing monkey to the mobile splash page

--- frontend/src/components/MobileSplashPage/MobileSplashPage.js
+++ frontend/src/components/MobileSplashPage/MobileSplashPage.js
@@ -1,6 +1,6 @@
 import './MobileSplashPage.css';
 
-// import SpinningMonkey from "../../../img/favpng_infinite-monkey-theorem3.png"
+import TypingMonkey from "../../img/favpng_infinite-monkey-theorem3.png"
 
 import MonkeySpinner from '../LoadingPageParts/monkey-spinner/MonkeySpinner';
 
@@ -8,8 +8,8 @@ const MobileSplashPage = (props) => {
 
   return (
     <div className="mobile-splash-container">
-      <div className='spinner-container'>
-        <MonkeySpinner/>
+      <div className='typing-monkey-container'>
+        <img className='mobile-splash-typing' src={TypingMonkey}/>
       </div>
       <div className="mobile-splash-title">
         Hello Mobile User!
@@ -25,6 +25,9 @@ const MobileSplashPage = (props) => {
       <div className='mobile-splash-page-video-cta'>
         <div>Check Out A video Introduction to 1000M</div>
         <div> <a href='https://gumlet.tv/playlist/684584bf0f8d7a0518536218/' target="_blank" rel="noopener noreferrer">HERE</a></div>
+      </div>
+      <div className='spinner-container'>
+        <MonkeySpinner/>
       </div>
     </div>
   )
